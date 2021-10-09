3d97a9b01fdd1004cbeff2ae8e0861bcfb7d34ff
Daniel Xie <danielyxie2002@gmail.com>
2021-10-08 23:13:24 -0400

    Dummy Enemies

---
+++ Fall2021Jam/Assets/Scripts/DummyEnemy.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyEnemy : Entity
+{
+    [SerializeField] private float cycleLength = 1.0f;
+    [SerializeField] private float baseMoveSpeed = 5.0f;
+    private float countdown;
+    private Vector3 dirVect;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdown = -1;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // every 3 seconds, randoly choose a direction to move in
+        if (countdown > 0) countdown -= Time.deltaTime;
+        else {
+            countdown = cycleLength;
+            dirVect = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), 0).normalized;
+        }
+        rb.MovePosition(rb.transform.position + dirVect * baseMoveSpeed * Time.deltaTime);
+    }
+}
